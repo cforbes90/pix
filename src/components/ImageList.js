e00d67bf306a7277c4360809f50b3f9e0870e82f
Shrfu31 <Shrfu31@Mac-Pro.localdomain>
2020-08-05 16:47:41 -0400

    Last commit before we change destructuring into a component.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import "./ImageList.css";
 
+import React from "react";
+import ImageCard from "./ImageCard";
 const ImageList = (props) => {
-  console.log(props.images);
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+  //   console.log(props.images);
+  //We can destructure the return so we don't have to put image.DOT.id, imageDOT.description
+  //To do this we need to add in those curly braces
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} alt={description} src={urls.regular} />;
   });
-  return <div> Image List: {images}</div>;
+  return <div className="image-list"> Image List: {images}</div>;
 };
 
 export default ImageList;
