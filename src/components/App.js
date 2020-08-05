6c81c61887d4ac3b9c17f8357d89558d789f08a3
Shrfu31 <Shrfu31@Mac-Pro.localdomain>
2020-08-05 15:25:24 -0400

    Added unsplash, ImageList component, basic rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 import { Icon } from "semantic-ui-react";
+import ImageList from "./ImageList";
 class App extends React.Component {
-  onSearchSubmit(term) {
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
     console.log(term);
-  }
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+    this.setState({ images: response.data.results });
+    console.log(this.state.images);
+    //   .then((response) => {
+    //     console.log(response.data.results);
+    //   });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         {" "}
         App <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+        Found: {this.state.images.length} images
         <Icon name="home"></Icon>
       </div>
     );
